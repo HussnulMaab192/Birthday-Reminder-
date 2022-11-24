2e9d89cf0b562b7532a43e00c1a58c406e019b93
Hussnul Maab <Hussnul.M@devnatives.com>
2022-11-24 18:22:47 +0500

    fix: dark and light theme using GetStorage

--- lib/widgets/theme_services.dart
+++ lib/widgets/theme_services.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:get_storage/get_storage.dart';
-
 class ThemeServices {
   final _box = GetStorage();
   final _key = "isDarkMode";
